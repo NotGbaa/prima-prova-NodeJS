a1859d9247cb8f56c623c50bede6d57dc28b40bd
Gabriele <gabriele@dibasoft.it>
2024-01-09 10:29:46 +0100

    prova

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //require è la stessa cosa dell'import 
-let http = reqire("http");
+let http = require("http");
 http.createServer(function (request, response) {
     response.writeHead(200, { 'content-type': "text/plane" });
     response.end("Hello world\n");
-}).listen(8080);
-console.log("server running on port 8080");
+}).listen(8081);
+console.log("server running on port 8081");
